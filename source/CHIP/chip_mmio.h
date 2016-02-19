515c5f7029e895ee5ddf7d3222c50eb84854aabe
jakobs <jakobs@novotek.com>
2016-02-19 15:52:32 +0100

    Changed names in c and h files to chip

--- source/CHIP/chip_mmio.h
+++ source/CHIP/chip_mmio.h
@@ -21,8 +21,8 @@
 // SOFTWARE.
 
 // Simple fast memory-mapped GPIO library for the Raspberry Pi.
-#ifndef PI_MMIO_H
-#define PI_MMIO_H
+#ifndef CHIP_MMIO_H
+#define CHIP_MMIO_H
 
 #include <stdint.h>
 
@@ -30,28 +30,28 @@
 #define MMIO_ERROR_DEVMEM -1
 #define MMIO_ERROR_MMAP -2
 
-volatile uint32_t* pi_mmio_gpio;
+volatile uint32_t* chip_mmio_gpio;
 
-int pi_mmio_init(void);
+int chip_mmio_init(void);
 
-static inline void pi_mmio_set_input(const int gpio_number) {
+static inline void chip_mmio_set_input(const int gpio_number) {
   // Set GPIO register to 000 for specified GPIO number.
-  *(pi_mmio_gpio+((gpio_number)/10)) &= ~(7<<(((gpio_number)%10)*3));
+  *(chip_mmio_gpio+((gpio_number)/10)) &= ~(7<<(((gpio_number)%10)*3));
 }
 
-static inline void pi_mmio_set_output(const int gpio_number) {
+static inline void chip_mmio_set_output(const int gpio_number) {
   // First set to 000 using input function.
   pi_mmio_set_input(gpio_number);
   // Next set bit 0 to 1 to set output.
-  *(pi_mmio_gpio+((gpio_number)/10)) |=  (1<<(((gpio_number)%10)*3));
+  *(chip_mmio_gpio+((gpio_number)/10)) |=  (1<<(((gpio_number)%10)*3));
 }
 
-static inline void pi_mmio_set_high(const int gpio_number) {
-  *(pi_mmio_gpio+7) = 1 << gpio_number;
+static inline void chip_mmio_set_high(const int gpio_number) {
+  *(chip_mmio_gpio+7) = 1 << gpio_number;
 }
 
-static inline void pi_mmio_set_low(const int gpio_number) {
-  *(pi_mmio_gpio+10) = 1 << gpio_number;
+static inline void chip_mmio_set_low(const int gpio_number) {
+  *(chip_mmio_gpio+10) = 1 << gpio_number;
 }
 
 static inline uint32_t pi_mmio_input(const int gpio_number) {
